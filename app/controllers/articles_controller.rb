f19996e22fdc7b9da73ac366c5516aa588f45089
Gourav10yrs <gourav.agarwal569@gmail.com>
2020-12-11 00:05:25 +0530

    Edit and update controller added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
+        # we pass article_params to whitelist the parameter
         @article = Article.new(article_params)
         if @article.save
             #to show message that article was created
@@ -21,7 +22,22 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was Successfully Updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
   private
+  # We are whitelisting the data that are comming from params.
     def article_params
         params.require(:article).permit(:title, :description)
     end
