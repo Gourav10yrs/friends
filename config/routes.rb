cb57849b7fa5d0dc672b27afa0b1fb5f2edea74f
Gourav10yrs <gourav.agarwal569@gmail.com>
2020-12-15 16:27:43 +0530

    UI for Signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root to: 'welcome#home'
   get 'about',  to: 'welcome#about'
 
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
   resources :articles
 end
