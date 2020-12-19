53339a5a6f9489a6b6f0c3fb9730e0eb105f3ffd
Gourav10yrs <gourav.agarwal569@gmail.com>
2020-12-19 14:37:44 +0530

    Show all user linked the user to their article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
 
     def new
         @user = User.new
@@ -27,6 +30,11 @@ class UsersController < ApplicationController
             render 'edit'
         end
     end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
 
     private 
     def user_params
